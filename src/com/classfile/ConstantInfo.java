ae6da7a7133b5f48ef3f896d2599e914da26861a
huangzhenghua <695143140@qq.com>
2017-05-22 15:13:05 +0800

    3.3.3

--- src/com/classfile/ConstantInfo.java
+++ src/com/classfile/ConstantInfo.java
@@ -1,5 +1,7 @@
 package com.classfile;
 
+import java.io.UnsupportedEncodingException;
+
 public abstract class ConstantInfo {
 	public static final byte CONSTANT_Class = 7;
 	public static final byte CONSTANT_Fieldref = 9;
@@ -27,48 +29,95 @@ public abstract class ConstantInfo {
 		return null;
 	}
 }
-class ConstantLongInfo extends ConstantInfo{
 
+class ConstantIntegerInfo extends ConstantInfo{
+	private int val;
 	@Override
 	public void readInfo(ClassReader cr) {
-		// TODO Auto-generated method stub
-		
+		this.val = cr.readUnit32();
 	}
-	
+}
+class ConstantFloatInfo extends ConstantInfo{
+	private float val;
+	@Override
+	public void readInfo(ClassReader cr) {
+		this.val = Float.intBitsToFloat(cr.readUnit32());
+	}
+}
+class ConstantLongInfo extends ConstantInfo{
+	private long val;
+	@Override
+	public void readInfo(ClassReader cr) {
+		this.val = cr.readUnit64();
+	}
 }
 class ConstantDoubleInfo extends ConstantInfo{
-
+	private double val;
 	@Override
 	public void readInfo(ClassReader cr) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-}
-class ConstantClassInfo extends ConstantInfo{
-	protected short nameIndex;
-
-	@Override
-	public void readInfo(ClassReader cr) {
-		// TODO Auto-generated method stub
-		
+		this.val = Double.longBitsToDouble(cr.readUnit64());
 	}
 }
 class ConstantUtf8Info extends ConstantInfo{
-
+	private String val;
 	@Override
 	public void readInfo(ClassReader cr) {
-		// TODO Auto-generated method stub
-		
+		int length = cr.readUnit16()&0xff;
+		byte[] bytes = cr.readBytes(length);
+		try {
+			this.val = new String(bytes,"utf8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 	}
-	
+}
+class ConstantStringInfo extends ConstantInfo{
+	private ConstantPool cp;
+	private short stringIndex;
+	@Override
+	public void readInfo(ClassReader cr) {
+		this.stringIndex = cr.readUnit16();
+	}
+	public String getString() {
+		return this.cp.getUtf8(this.stringIndex);
+	}
+}
+class ConstantClassInfo extends ConstantInfo{
+	private ConstantPool cp;
+	protected short nameIndex;
+	@Override
+	public void readInfo(ClassReader cr) {
+		this.nameIndex = cr.readUnit16();
+	}
+	public String getName(){
+		return this.cp.getUtf8(this.nameIndex);
+	}
 }
 class ConstantNameAndTypeInfo extends ConstantInfo{
 	protected short nameIndex;
 	protected short descriptionIndex;
 	@Override
 	public void readInfo(ClassReader cr) {
-		// TODO Auto-generated method stub
-		
+		this.nameIndex = cr.readUnit16();
+		this.descriptionIndex = cr.readUnit16();
 	}
 }
+class ConstantMemberrefInfo extends ConstantInfo{
+	private ConstantPool cp;
+	protected short classIndex;
+	protected short nameAndTypeIndex;
+	@Override
+	public void readInfo(ClassReader cr) {
+		this.classIndex = cr.readUnit16();
+		this.nameAndTypeIndex = cr.readUnit16();
+	}
+	public String getClassName(){
+		return this.cp.getUtf8(classIndex);
+	}
+	public String getNameAndDescription(){
+		return this.cp.getUtf8(nameAndTypeIndex);
+	}
+}
+class ConstantFieldrefInfo extends ConstantMemberrefInfo{}
+class ConstantMethodrefInfo extends ConstantMemberrefInfo{}
+class ConstantInterfaceMethodrefInfo extends ConstantMemberrefInfo{}
